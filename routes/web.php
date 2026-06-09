ba2f937d09d2ee3bf5111202fc4076f9d8ba4371
Bat`s <83263245+ChristianDaveMomarBate@users.noreply.github.com>
2026-06-09 09:48:31 +0800

    fix routing starting

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-require __DIR__.'/auth.php';
+use App\Http\Controllers\HomeController;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Root URL -> Login page
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+// Dashboard (accessible after login)
+Route::get('/dashboard', [HomeController::class, 'index'])
+    ->middleware('auth')
+    ->name('dashboard');
